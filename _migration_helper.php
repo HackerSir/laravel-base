26e2ec139aea79903b353c457bb27d15bb2f44e4
KID <jyhsu2000@yahoo.com.tw>
2016-08-13 15:40:42 +0800

    Add migration helper

---
+++ _migration_helper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Illuminate\Support;
+
+/**
+ * @method Fluent first()
+ * @method Fluent after($column)
+ * @method Fluent change()
+ * @method Fluent nullable()
+ * @method Fluent unsigned()
+ * @method Fluent unique()
+ * @method Fluent index()
+ * @method Fluent primary()
+ * @method Fluent default($value)
+ * @method Fluent onUpdate($value)
+ * @method Fluent onDelete($value)
+ * @method Fluent references($value)
+ * @method Fluent on($value)
+ */
+class Fluent
+{
+}
