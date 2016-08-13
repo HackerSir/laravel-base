0c4f6f5e5884499d7c2dab03d54d84f679186741
KID <jyhsu2000@yahoo.com.tw>
2016-08-13 16:46:50 +0800

    Fxi form method

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <h2 class="ui teal header center aligned">編輯個人資料</h2>
-    {!! SemanticForm::open()->patch()->action(route('profile.update'))->addClass('large') !!}
+    {!! SemanticForm::open()->put()->action(route('profile.update'))->addClass('large') !!}
     {!! SemanticForm::bind($user) !!}
     <div class="ui stacked segment">
         {!! SemanticForm::text('email')->label('Email')->placeholder('Email')->disable() !!}
