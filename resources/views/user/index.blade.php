a8dd516c71706b6ac65d52f444359f9a40bb2087
KID <jyhsu2000@yahoo.com.tw>
2016-08-17 03:00:13 +0800

    Update background of pagination bar

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -53,7 +53,7 @@
         @endforeach
         </tbody>
     </table>
-    <div class="ui center aligned attached segment" style="border: none">
+    <div class="ui center aligned attached segment" style="border: none; background: none">
         {!! (new Landish\Pagination\SemanticUI($users))->render() !!}
     </div>
 @endsection
