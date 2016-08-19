b54a496cfe78eba99285bd1e5cf848b21c35d4ea
KID <jyhsu2000@yahoo.com.tw>
2016-08-19 13:00:46 +0800

    Create UsersTableSeeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,10 +12,20 @@
 */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
+    $dataTime = $faker->dateTime;
+    $ip = ($faker->boolean) ? $faker->ipv4 : $faker->ipv6;
+    $isConfirmed = $faker->boolean(80);
+
     return [
         'name'           => $faker->name,
         'email'          => $faker->safeEmail,
         'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'confirm_code'   => (!$isConfirmed) ? str_random(60) : '',
+        'confirm_at'     => ($isConfirmed) ? $dataTime : null,
+        'register_at'    => $dataTime,
+        'register_ip'    => $ip,
+        'last_login_at'  => $dataTime,
+        'last_login_ip'  => $ip,
     ];
 });
