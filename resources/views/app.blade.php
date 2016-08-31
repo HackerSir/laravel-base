24d8676003100ab6500e74a988dc50e26110b5b2
KID <jyhsu2000@yahoo.com.tw>
2016-08-31 23:51:09 +0800

    Add Google Analysis

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -95,6 +95,22 @@
                 position: 'right center'
             });
         });
+        // Google分析
+        @if(env('GOOGLE_ANALYSIS'))
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', '{{ env('GOOGLE_ANALYSIS') }}', 'auto');
+        ga('send', 'pageview');
+        @endif
     });
 </script>
 @yield('js')
