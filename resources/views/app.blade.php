e052359f1a1339f6b8fc256061b8b6ccf31da99a
KID <jyhsu2000@yahoo.com.tw>
2016-11-16 13:45:10 +0800

    Update for standard page template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+    {{-- Metatag --}}
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <title>@yield('title')::{{ config('site.name') }}</title>
+
+    {{-- CSS --}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"
+          integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.8.0/css/alertify.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/alertifyjs/1.8.0/css/themes/bootstrap.min.css"/>
+    <style>
+
+    </style>
+    @yield('css')
+</head>
+<body>
+{{-- TODO: Navbar --}}
+{{--@include('navbar.menu')--}}
+
+
+{{-- Content --}}
+<div class="container">
+    @yield('content')
+</div>
+
+{{-- TODO: Footer --}}
+
+{{-- Javascript --}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"
+        integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"
+        integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"
+        integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK"
+        crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/alertifyjs/1.8.0/alertify.min.js"></script>
+<script>
+    $(function () {
+        //AlertifyJS
+        alertify.defaults = {
+            notifier: {
+                position: 'top-right'
+            }
+        };
+        @if(session('global'))
+            alertify.notify('{{ session('global') }}', 'success', 5);
+        @endif
+        @if(session('warning'))
+            alertify.notify('{{ session('warning') }}', 'warning', 5);
+        @endif
+        // Tooltip
+        $('[title]:not(#tracy-debug *[title])').each(function () {
+            $(this).tooltip({
+                placement: 'right'
+            });
+        });
+        // Google分析
+        @if(env('GOOGLE_ANALYSIS'))
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', '{{ env('GOOGLE_ANALYSIS') }}', 'auto');
+        ga('send', 'pageview');
+        @endif
+    });
+</script>
+@yield('js')
+
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('app')
+
+@section('title', '首頁')
+
+@section('css')
+    <style>
+        .jumbotron {
+            text-align: center;
+            background: rgba(100, 100, 100, .6);
+            margin-top: 20vh;
+            padding-top: 40px;
+            border-radius: 20px;
+        }
+    </style>
+@endsection
+@section('content')
+    <div class="jumbotron">
+        <h1>{{ config('site.name') }}</h1>
+        <h2>逢甲大學黑客社</h2>
+        <a href="javascript:void(0)" class="btn btn-primary btn-lg" style="margin-top: 5vh;" title="Let's GO!!">GO!</a>
+    </div>
+@endsection
