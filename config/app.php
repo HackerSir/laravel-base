93867093675073f4888666e222f64b785ec1d996
KID <jyhsu2000@yahoo.com.tw>
2016-11-16 14:56:49 +0800

    Install recca0120/laravel-tracy

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
 
         Collective\Html\HtmlServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Recca0120\LaravelTracy\ServiceProvider::class,
 
         /*
          * Application Service Providers...
---
+++ config/terminal.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'enabled'    => true,
+    'whitelists' => [],
+    'route'      => [
+        'prefix'     => 'terminal',
+        'as'         => 'terminal.',
+        'middleware' => ['web'],
+    ],
+    'commands'   => [
+        \Recca0120\Terminal\Console\Commands\Artisan::class,
+        \Recca0120\Terminal\Console\Commands\ArtisanTinker::class,
+        \Recca0120\Terminal\Console\Commands\Cleanup::class,
+        \Recca0120\Terminal\Console\Commands\Find::class,
+        \Recca0120\Terminal\Console\Commands\Mysql::class,
+        \Recca0120\Terminal\Console\Commands\Tail::class,
+        \Recca0120\Terminal\Console\Commands\Vi::class,
+    ],
+];
