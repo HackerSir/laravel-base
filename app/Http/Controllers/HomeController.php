bba88dcb5a132bd4d162a960a285b92eab42cdf7
KID <jyhsu2000@yahoo.com.tw>
2016-11-16 06:38:37 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     /**
@@ -11,7 +9,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-//        $this->middleware('auth');
+        //        $this->middleware('auth');
     }
 
     /**
