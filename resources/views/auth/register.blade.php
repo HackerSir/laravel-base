b63ba92d12158f46b75260ae01392a00a68acaae
KID <jyhsu2000@yahoo.com.tw>
2016-11-17 11:17:24 +0800

    Update login and register page to bootstrap 4

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                 {{ csrf_field() }}
 
                 <div class="form-group row{{ $errors->has('name') ? ' has-error' : '' }}">
-                    <label for="name" class="col-md-4 control-label">Name</label>
+                    <label for="name" class="col-md-4 form-control-label">Name</label>
 
                     <div class="col-md-6">
                         <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required
@@ -27,7 +27,7 @@
                 </div>
 
                 <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    <label for="email" class="col-md-4 form-control-label">E-Mail Address</label>
 
                     <div class="col-md-6">
                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
@@ -42,7 +42,7 @@
                 </div>
 
                 <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">Password</label>
+                    <label for="password" class="col-md-4 form-control-label">Password</label>
 
                     <div class="col-md-6">
                         <input id="password" type="password" class="form-control" name="password" required>
@@ -56,7 +56,7 @@
                 </div>
 
                 <div class="form-group row">
-                    <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                    <label for="password-confirm" class="col-md-4 form-control-label">Confirm Password</label>
 
                     <div class="col-md-6">
                         <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
