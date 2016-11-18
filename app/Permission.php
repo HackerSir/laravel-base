7a2e67daadaa7b134495653c35be7e91a1db6892
KID <jyhsu2000@yahoo.com.tw>
2016-11-18 15:55:10 +0800

    Generate phpDoc by using php artisan ide-helper:models

--- app/Permission.php
+++ app/Permission.php
@@ -5,6 +5,24 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Klaravel\Ntrust\Traits\NtrustPermissionTrait;
 
+/**
+ * App\Permission
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $display_name
+ * @property string $description
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Role[] $roles
+ * @method static \Illuminate\Database\Query\Builder|\App\Permission whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Permission whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Permission whereDisplayName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Permission whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Permission whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Permission extends Model
 {
     use NtrustPermissionTrait;
--- app/User.php
+++ app/User.php
@@ -6,6 +6,41 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Klaravel\Ntrust\Traits\NtrustUserTrait;
 
+/**
+ * App\User
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $confirm_code
+ * @property string $confirm_at
+ * @property string $register_at
+ * @property string $register_ip
+ * @property string $last_login_at
+ * @property string $last_login_ip
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $readNotifications
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $unreadNotifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Role[] $roles
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereConfirmCode($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereConfirmAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRegisterAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRegisterIp($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereLastLoginIp($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
