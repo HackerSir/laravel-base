d1d7791dd9c2bc48dab5690a22ddcf20a9c3ce0b
KID <jyhsu2000@yahoo.com.tw>
2016-11-19 16:32:59 +0800

    Add protection field in roles and permissions

--- database/migrations/2016_11_18_000852_user_ntrust_setup_tables.php
+++ database/migrations/2016_11_18_000852_user_ntrust_setup_tables.php
@@ -18,6 +18,7 @@ class UserNtrustSetupTables extends Migration
             $table->string('name')->unique();
             $table->string('display_name')->nullable();
             $table->string('description')->nullable();
+            $table->boolean('protection')->default(true);
             $table->timestamps();
         });
 
@@ -40,6 +41,7 @@ class UserNtrustSetupTables extends Migration
             $table->string('name')->unique();
             $table->string('display_name')->nullable();
             $table->string('description')->nullable();
+            $table->boolean('protection')->default(true);
             $table->timestamps();
         });
 
