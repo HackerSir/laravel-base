19389a025e6a6751c784672a1379b993a3c713e4
KID <jyhsu2000@yahoo.com.tw>
2016-11-21 12:55:47 +0800

    Use APP_NAME as Session Cookie Name to prevent conflict with other project

--- config/session.php
+++ config/session.php
@@ -122,7 +122,7 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session',
+    'cookie' => env('APP_NAME', 'laravel_session'),
 
     /*
     |--------------------------------------------------------------------------
