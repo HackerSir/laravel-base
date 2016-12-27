3c2b0e7fdb7be115d118120f631bd3871f5ffe32
KID <jyhsu2000@yahoo.com.tw>
2016-12-28 01:56:51 +0800

    Composer update

--- resources/views/vendor/log-viewer/show.blade.php
+++ resources/views/vendor/log-viewer/show.blade.php
@@ -106,7 +106,7 @@
                                     <tr>
                                         <td colspan="5" class="stack">
                                             <div class="stack-content collapse" id="log-stack-{{ $key }}">
-                                                {!! preg_replace("/\n/", '<br>', $entry->stack) !!}
+                                                {!! $entry->stack() !!}
                                             </div>
                                         </td>
                                     </tr>
@@ -128,7 +128,9 @@
             </div>
         </div>
     </div>
+@endsection
 
+@section('modals')
     {{-- DELETE MODAL --}}
     <div id="delete-log-modal" class="modal fade">
         <div class="modal-dialog">
@@ -163,7 +165,7 @@
                 deleteLogForm  = $('form#delete-log-form'),
                 submitBtn      = deleteLogForm.find('button[type=submit]');
 
-            deleteLogForm.submit(function(event) {
+            deleteLogForm.on('submit', function(event) {
                 event.preventDefault();
                 submitBtn.button('loading');
 
