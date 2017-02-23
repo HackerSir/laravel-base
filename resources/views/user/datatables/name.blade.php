eb47655324acf83a9f5c3c4aa46f7364edd4dc4d
KID <jyhsu2000@yahoo.com.tw>
2017-02-23 22:30:00 +0800

    Change tag to badge

--- resources/views/user/datatables/name.blade.php
+++ resources/views/user/datatables/name.blade.php
@@ -2,6 +2,6 @@
 <span style="font-size: 2em">
     {{ link_to_route('user.show', $name, $id) }}
     @foreach($roles as $role)
-        <span class="tag tag-default">{{ $role['display_name'] }}</span>
+        <span class="badge badge-default">{{ $role['display_name'] }}</span>
     @endforeach
 </span>
