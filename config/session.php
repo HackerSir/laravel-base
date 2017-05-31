be70c1c16363b92cba0ddf90d9aee29886d5e4b0
KID <jyhsu2000@yahoo.com.tw>
2017-05-31 20:03:39 +0800

    Add APP_PREFIX in .env to make some prefix editable

--- config/session.php
+++ config/session.php
@@ -122,7 +122,7 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session',
+    'cookie' => env('APP_PREFIX', 'laravel') . '_session',
 
     /*
     |--------------------------------------------------------------------------
