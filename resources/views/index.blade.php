3c630e528217fd139118d67526d263b2cc5cf968
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-03 21:50:23 +0800

    Change index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,20 +5,18 @@
 @section('css')
     <style>
         .jumbotron {
-            text-align: center;
             word-break: break-all;
-            background: rgba(100, 100, 100, .6);
-            margin-top: 20vh;
             margin-bottom: 0;
-            padding-top: 40px;
             border-radius: 20px;
+            padding-top: 48px;
+            padding-bottom: 48px;
         }
     </style>
 @endsection
 @section('content')
-    <div class="jumbotron">
-        <h1 class="display-1">{{ config('app.name') }}</h1>
-        <h2 class="display-3">逢甲大學黑客社</h2>
-        <a href="javascript:void(0)" class="btn btn-primary btn-lg" style="margin-top: 5vh;" title="Let's GO!!">GO!</a>
+    <div class="jumbotron mt-3">
+        <h1>{{ config('app.name') }}</h1>
+        <h3>逢甲大學黑客社</h3>
+        <a href="javascript:void(0)" class="btn btn-primary btn-lg" title="Let's GO!!">GO!</a>
     </div>
 @endsection
