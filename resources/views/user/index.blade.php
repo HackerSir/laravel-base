4cdd5bf066743899d397cdfafc14244f92e036ad
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-03 20:49:22 +0800

    Change user view

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,8 +3,14 @@
 @section('title', '會員清單')
 
 @section('content')
-    <h1>會員清單</h1>
-    {!! $dataTable->table() !!}
+    <div class="mt-3 pb-3">
+        <h1>會員清單</h1>
+        <div class="card">
+            <div class="card-block">
+                {!! $dataTable->table() !!}
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('js')
