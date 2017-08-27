aad905ae2ca144b60140fb58060719f645f7fccc
Chan, Danny <danny50610@yahoo.com.tw>
2017-08-27 21:09:55 +0800

    Fix register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,8 +3,8 @@
 @section('title', '註冊')
 
 @section('content')
-    <div class="row mt-3">
-        <div class="col-md-8 offset-md-2">
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-8">
             <h1>註冊</h1>
             <div class="card">
                 <div class="card-body">
@@ -73,7 +73,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-10 offset-md-2">
+                            <div class="col-md-10 ml-auto">
                                 <button type="submit" class="btn btn-primary">
                                     註冊
                                 </button>
