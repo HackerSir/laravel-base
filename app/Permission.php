b77a908b96810750149089872271d685c7c0a820
KID <jyhsu2000@yahoo.com.tw>
2017-09-21 03:19:24 +0800

    Update phpDoc

--- app/Permission.php
+++ app/Permission.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Laratrust\LaratrustPermission;
 
+/**
+ * App\Permission
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Role[] $roles
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Permission whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Permission extends LaratrustPermission
 {
     protected $fillable = [
--- app/Role.php
+++ app/Role.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Laratrust\LaratrustRole;
 
+/**
+ * App\Role
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $display_name
+ * @property string|null $description
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Permission[] $permissions
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Role extends LaratrustRole
 {
     protected $fillable = [
