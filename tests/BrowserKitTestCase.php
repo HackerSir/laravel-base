c691fde076ba4c4840c8dcdad4568d8721694d84
danny50610 <danny50610@yahoo.com.tw>
2017-10-05 12:13:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/BrowserKitTestCase.php
+++ tests/BrowserKitTestCase.php
@@ -9,11 +9,12 @@ abstract class BrowserKitTestCase extends BaseTestCase
     use CreatesApplication;
     public $baseUrl = 'http://localhost';
 
-    public function stringToxxxx($str) {
+    public function stringToxxxx($str)
+    {
         $arrayStr = preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
         $arrayResult = array_map(function ($T) {
             // http://help.i2yes.com/?q=node%2F186
-            return '&#' . base_convert(bin2hex(iconv("utf-8", "ucs-4", $T)), 16, 10) . ';';
+            return '&#' . base_convert(bin2hex(iconv('utf-8', 'ucs-4', $T)), 16, 10) . ';';
         }, $arrayStr);
 
         return implode($arrayResult);
