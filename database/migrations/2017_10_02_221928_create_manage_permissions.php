71ba1eeaad944eddd00c50b6137de8d0663be5fd
KID <jyhsu2000@yahoo.com.tw>
2018-03-01 17:15:14 +0800

    Remove invalid properties in create

--- database/migrations/2017_10_02_221928_create_manage_permissions.php
+++ database/migrations/2017_10_02_221928_create_manage_permissions.php
@@ -17,35 +17,30 @@ class CreateManagePermissions extends Migration
             'name'         => 'menu.view',
             'display_name' => '顯示管理選單',
             'description'  => '顯示所有網站管理用的選單',
-            'protection'   => true,
         ]);
 
         $permUserManage = Permission::create([
             'name'         => 'user.manage',
             'display_name' => '管理會員',
             'description'  => '修改會員資料、調整會員權限、刪除會員等',
-            'protection'   => false,
         ]);
 
         $permUserView = Permission::create([
             'name'         => 'user.view',
             'display_name' => '查看會員資料',
             'description'  => '查看會員清單、資料、權限等',
-            'protection'   => false,
         ]);
 
         $permLogViewerAccess = Permission::create([
             'name'         => 'log-viewer.access',
             'display_name' => '進入 Log Viewer 面板',
             'description'  => '進入 Log Viewer 面板，對網站記錄進行查詢與管理',
-            'protection'   => false,
         ]);
 
         $permRoleManage = Permission::create([
             'name'         => 'role.manage',
             'display_name' => '管理角色',
             'description'  => '新增、修改、刪除角色',
-            'protection'   => false,
         ]);
 
         // Find Admin and give permission to him
