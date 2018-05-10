70d2510ccdf86acad28192e3158f7e28f76f351a
Chan, Danny <danny50610@yahoo.com.tw>
2018-05-10 19:56:58 +0800

    Update model phpdoc

--- app/User.php
+++ app/User.php
@@ -5,6 +5,12 @@ namespace App;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
