1789674b31c32433d141278f86e714aff3a4a4a8
Chan, Danny <danny50610@yahoo.com.tw>
2018-05-17 21:48:23 +0800

    Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 /*
@@ -13,11 +14,20 @@ use Faker\Generator as Faker;
 |
 */
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker $faker) {
+    static $password;
     return [
         'name'           => $faker->name,
         'email'          => $faker->unique()->safeEmail,
-        'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'confirm_at'     => Carbon::now(),
+    ];
+});
+
+$factory->state(App\User::class, 'NoMailConfirm', function ($faker) {
+    return [
+        'confirm_at' => null,
     ];
 });
