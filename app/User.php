d3be573da4589e3a0aaaf2ebb41e189ed0047750
KID <jyhsu2000@yahoo.com.tw>
2018-05-17 17:43:49 +0800

    Create User

--- app/User.php
+++ app/User.php
@@ -8,7 +8,34 @@ use Illuminate\Notifications\Notifiable;
 /**
  * App\User
  *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $confirm_code
+ * @property string|null $confirm_at
+ * @property string|null $register_at
+ * @property string|null $register_ip
+ * @property string|null $last_login_at
+ * @property string|null $last_login_ip
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read bool $is_confirmed
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereConfirmAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereConfirmCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLastLoginIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRegisterAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRegisterIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class User extends Authenticatable
@@ -21,7 +48,15 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'confirm_code',
+        'confirm_at',
+        'register_at',
+        'register_ip',
+        'last_login_at',
+        'last_login_ip',
     ];
 
     /**
@@ -32,4 +67,18 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $appends = [
+        'is_confirmed',
+    ];
+
+    /**
+     * 帳號是否完成驗證
+     *
+     * @return bool
+     */
+    public function getIsConfirmedAttribute()
+    {
+        return !empty($this->confirm_at);
+    }
 }
