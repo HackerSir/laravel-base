4d09a4a00cfcaed97642be13d03860561d60ef33
KID <jyhsu2000@yahoo.com.tw>
2018-10-22 14:18:38 +0800

    Install lavary/laravel-menu

---
+++ app/Http/Middleware/LaravelMenu.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Lavary\Menu\Builder;
+use Menu;
+
+class LaravelMenu
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //左側
+        Menu::make('left', function (Builder $menu) {
+        });
+
+        Menu::make('right', function (Builder $menu) {
+            if (auth()->check()) {
+                // 會員
+
+//                // 信箱驗證
+//                if (!auth()->user()->is_confirmed) {
+//                    $menu->add('尚未完成信箱驗證', ['route' => 'confirm-mail.resend'])
+//                        ->link->attr(['class' => 'text-danger']);
+//                }
+//
+//                // 管理員
+//                if (Laratrust::can('menu.view') and auth()->user()->isConfirmed) {
+//                    /** @var \Lavary\Menu\Builder $adminMenu */
+//                    $adminMenu = $menu->add('管理選單', 'javascript:void(0)');
+//
+//                    if (Laratrust::can(['user.manage', 'user.view'])) {
+//                        $adminMenu->add('會員管理', ['route' => 'user.index'])->active('user/*');
+//                    }
+//                    if (Laratrust::can('role.manage')) {
+//                        $adminMenu->add('角色管理', ['route' => 'role.index']);
+//                    }
+//
+//                    if (Laratrust::can('log-viewer.access')) {
+//                        $adminMenu->add(
+//                            '記錄檢視器 <i class="fas fa-external-link-alt" aria-hidden="true"></i>',
+//                            ['route' => 'log-viewer::dashboard']
+//                        )->link->attr('target', '_blank');
+//                    }
+//                }
+
+                /** @var \Lavary\Menu\Builder $userMenu */
+                $userMenu = $menu->add(auth()->user()->name, 'javascript:void(0)');
+//                $userMenu->add('個人資料', ['route' => 'profile'])->active('profile/*');
+                $userMenu->add('登出', ['route' => 'logout']);
+            } else {
+                // 遊客
+                $menu->add('登入', ['route' => 'login']);
+            }
+        });
+
+        return $next($request);
+    }
+
+    protected function addDivider(\Lavary\Menu\Item $subMenu)
+    {
+        $lastItem = $subMenu->children()->last();
+        if ($lastItem) {
+            $lastItem->divide();
+        }
+    }
+}
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer bg-dark">
+    <div class="container">
+        <p class="text-muted mb-0">©2018 HackerSir. All rights reserved.</p>
+    </div>
+</footer>
