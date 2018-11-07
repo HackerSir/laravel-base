5cd56526d0ca57beb33ef31fb14b839d32a4984d
KID <jyhsu2000@yahoo.com.tw>
2018-11-07 14:52:26 +0800

    Add time and ip of register and last login to User

--- app/User.php
+++ app/User.php
@@ -18,6 +18,10 @@ use Laratrust\Traits\LaratrustUserTrait;
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $register_at 註冊時間
+ * @property string|null $register_ip 註冊IP
+ * @property string|null $last_login_at 最後登入時間
+ * @property string|null $last_login_ip 最後登入IP
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Permission[] $permissions
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Role[] $roles
@@ -25,9 +29,13 @@ use Laratrust\Traits\LaratrustUserTrait;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereLastLoginIp($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePermissionIs($permission = '')
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRegisterAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRegisterIp($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRoleIs($role = '')
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
