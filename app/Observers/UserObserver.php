34a08a1dbacbbe78be43f6582e9a66db44af95e0
KID <jyhsu2000@yahoo.com.tw>
2018-11-07 14:53:18 +0800

    Create UserObserver

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Observers;
+
+use App\User;
+use Carbon\Carbon;
+
+class UserObserver
+{
+    /**
+     * Handle the user "creating" event.
+     *
+     * @param  \App\User $user
+     * @return void
+     */
+    public function creating(User $user)
+    {
+        $user->register_at = Carbon::now();
+        $user->register_ip = request()->getClientIp();
+        $user->last_login_at = Carbon::now();
+        $user->last_login_ip = request()->getClientIp();
+    }
+}
