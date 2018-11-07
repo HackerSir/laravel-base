25eacd615f15c381adc353d501a9099d6147af9a
KID <jyhsu2000@yahoo.com.tw>
2018-11-07 14:53:10 +0800

    Create AuthListener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +17,15 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+    ];
+
+    /**
+     * 訂閱者類別進行註冊。
+     *
+     * @var array
+     */
+    protected $subscribe = [
+        \App\Listeners\AuthListener::class,
     ];
 
     /**
