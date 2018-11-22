87b79972286d05017b045a6df21fe346dddc0f41
KID <jyhsu2000@yahoo.com.tw>
2018-11-22 12:50:51 +0800

    Add register toggle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,9 @@
             <div class="row">
                 <div class="mx-auto">
                     {{ bs()->submit('登入', 'primary')->prependChildren(fa()->icon('check')->addClass('mr-2')) }}
-                    {{ bs()->a(route('register'), '註冊')->asButton('light')->prependChildren(fa()->icon('user-plus')->addClass('mr-2')) }}
+                    @if((bool)config('app-extend.allow-register'))
+                        {{ bs()->a(route('register'), '註冊')->asButton('light')->prependChildren(fa()->icon('user-plus')->addClass('mr-2')) }}
+                    @endif
                     {{ bs()->a(route('password.request'), '忘記密碼')->asButton('light')->prependChildren(fa()->icon('question-circle')->addClass('mr-2')) }}
                 </div>
             </div>
