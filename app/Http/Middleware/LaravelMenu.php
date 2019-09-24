cf26a97a44c798ea52264fdf6d563e4dde804495
KID <jyhsu2000@yahoo.com.tw>
2019-09-24 18:08:57 +0800

    Create manage menu

--- app/Http/Middleware/LaravelMenu.php
+++ app/Http/Middleware/LaravelMenu.php
@@ -34,12 +34,12 @@ class LaravelMenu
                     $menu->add('尚未完成信箱驗證', ['route' => 'verification.notice'])
                         ->link->attr(['class' => 'text-danger']);
                 }
-//
-//                // 管理員
-//                if (Laratrust::can('menu.view') && $user->hasVerifiedEmail()) {
-//                    /** @var \Lavary\Menu\Builder $adminMenu */
-//                    $adminMenu = $menu->add('管理選單', 'javascript:void(0)');
-//
+
+                // 管理員
+                if (Laratrust::can('menu.view') && $user->hasVerifiedEmail()) {
+                    /** @var \Lavary\Menu\Builder $adminMenu */
+                    $adminMenu = $menu->add('管理選單', 'javascript:void(0)');
+
 //                    if (Laratrust::can('user.manage')) {
 //                        $adminMenu->add('會員管理', ['route' => 'user.index'])->active('user/*');
 //                    }
@@ -60,7 +60,7 @@ class LaravelMenu
 //                            ['url' => config('telescope.path')]
 //                        )->link->attr('target', '_blank');
 //                    }
-//                }
+                }
 
                 /** @var \Lavary\Menu\Builder $userMenu */
                 $userMenu = $menu->add(auth()->user()->name, 'javascript:void(0)');
