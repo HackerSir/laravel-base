3ae6116be605a51902dd9905ff2f3d1a4e244d9b
KID <jyhsu2000@yahoo.com.tw>
2019-09-24 13:44:15 +0800

    Simply setup Auth with Email Verification

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laratrust\Traits\LaratrustUserTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use LaratrustUserTrait;
     use Notifiable;
--- config/laratrust.php
+++ config/laratrust.php
@@ -244,7 +244,7 @@ return [
              * If the message content is empty it won't be added to the redirection.
              */
             'redirect' => [
-                'url'     => '/home',
+                'url'     => '/',
                 'message' => [
                     'key'     => 'error',
                     'content' => '',
