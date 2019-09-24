55e313e93df758ece7c28e7a365c897357466beb
KID <jyhsu2000@yahoo.com.tw>
2019-09-24 12:00:50 +0800

    Setup Laratrust

---
+++ app/Permission.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Laratrust\Models\LaratrustPermission;
+
+class Permission extends LaratrustPermission
+{
+    protected $fillable = [
+        'name',
+        'display_name',
+        'description',
+    ];
+
+    /**
+     * @param $roleName
+     *
+     * @return bool
+     */
+    public function hasRole($roleName)
+    {
+        foreach ($this->roles as $role) {
+            if ($role->name == $roleName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ app/Role.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Laratrust\Models\LaratrustRole;
+
+class Role extends LaratrustRole
+{
+    protected $fillable = [
+        'name',
+        'display_name',
+        'description',
+        'protection',
+    ];
+
+    protected $casts = [
+        'protection' => 'boolean',
+    ];
+}
