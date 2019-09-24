c8d16c90a6976ae0868bee83ff01e0676f15267c
KID <jyhsu2000@yahoo.com.tw>
2019-09-24 11:57:18 +0800

    Setup LaravelMenu

---
+++ resources/views/components/navbar-subitems.blade.php
@@ -0,0 +1,15 @@
+@foreach($items as $item)
+    @if($item->link)
+        <a @lm_attrs($item->link) class="dropdown-item" @lm_endattrs href="{!! $item->url() !!}">
+            {!! $item->title !!}
+        </a>
+    @else
+        {!! $item->title !!}
+    @endif
+    {{--@if($item->hasChildren())--}}
+        {{----}}
+    {{--@endif--}}
+    @if($item->divider)
+        <div class="dropdown-divider"></div>
+    @endif
+@endforeach
