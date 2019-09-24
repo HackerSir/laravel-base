03cd3505f328a80af10a77c35402e65f851552ac
KID <jyhsu2000@yahoo.com.tw>
2019-09-24 13:58:30 +0800

    Apply fixes from StyleCI (#80)
    [ci skip] [skip ci]

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace App\Providers;
 
+use Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Event;
 
 class EventServiceProvider extends ServiceProvider
 {
