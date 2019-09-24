a0ea8cfebb28b0b7c2307261120ddb7c77acb2db
KID <jyhsu2000@yahoo.com.tw>
2019-09-24 13:46:39 +0800

    Remove Facades imports

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Hash;
+use Validator;
 
 class RegisterController extends Controller
 {
