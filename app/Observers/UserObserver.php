c91e13f8ea2dbd67f044bb7ffab5645a283443cb
KID <jyhsu2000@yahoo.com.tw>
2019-09-25 11:19:30 +0800

    Automatically assign Admin role to first user

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Role;
 use App\User;
 use Carbon\Carbon;
 
@@ -19,5 +20,20 @@ class UserObserver
         $user->register_ip = request()->getClientIp();
         $user->last_login_at = Carbon::now();
         $user->last_login_ip = request()->getClientIp();
+    }
+
+    /**
+     * Handle the user "created" event.
+     *
+     * @param \App\User $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        if (User::count() <= 1) {
+            // 若是第一個註冊的，直接給予管理員權限
+            $admin = Role::whereName('Admin')->first();
+            $user->attachRole($admin);
+        }
     }
 }
