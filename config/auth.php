3916b5c394acc0580ec3a2ac60098a9f6d65d2de
KID <jyhsu2000@yahoo.com.tw>
2019-10-16 15:25:38 +0800

    Update for password confirmation

--- config/auth.php
+++ config/auth.php
@@ -92,12 +92,25 @@ return [
     |
     */
 
-    'passwords' => [
+    'passwords'        => [
         'users' => [
             'provider' => 'users',
             'table'    => 'password_resets',
             'expire'   => 60,
         ],
     ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Confirmation Timeout
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the amount of seconds before a password confirmation
+    | times out and the user is prompted to re-enter their password via the
+    | confirmation screen. By default, the timeout lasts for three hours.
+    |
+    */
+    'password_timeout' => 10800,
 
 ];
