cd653fb10f6f2e9d86fe61b850b3ccf2a41dc385
火文狼 <vongola12324@coder.tw>
2021-02-23 15:01:11 +0800

    Apply fixes from StyleCI (#107)
    [ci skip] [skip ci]
    
    Co-authored-by: vongola12324 <vongola12324@users.noreply.github.com>

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -32,8 +32,8 @@ class NewPasswordController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'token' => 'required',
-            'email' => 'required|email',
+            'token'    => 'required',
+            'email'    => 'required|email',
             'password' => 'required|string|confirmed|min:8',
         ]);
 
@@ -44,7 +44,7 @@ class NewPasswordController extends Controller
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function ($user) use ($request) {
                 $user->forceFill([
-                    'password' => Hash::make($request->password),
+                    'password'       => Hash::make($request->password),
                     'remember_token' => Str::random(60),
                 ])->save();
 
