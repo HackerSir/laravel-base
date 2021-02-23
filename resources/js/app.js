6db9d6fb051de85d981f59ce404c8f66bdbd3d26
Vongola <vongola12324@coder.tw>
2021-02-23 13:10:41 +0800

    Add laravel-breeze and vue support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+import _ from 'lodash';
+import Vue from 'vue';
+import VueTailwind from 'vue-tailwind';
+import Vue2SimpleDatatable from 'vue2-simple-datatable';
+import components from './tailwindcss';
 require('./bootstrap');
+
+// Register Vue
+window.Vue = require('vue').default;
+
+Object.defineProperty(Vue.prototype, '$_', { value: _ });
+
+// Register Vue Plugins
+Vue.use(VueTailwind, components);
+
+// Create Instance
+// eslint-disable-next-line no-unused-vars
+const app = new Vue({
+  el: '#app',
+});
---
+++ resources/js/tailwindcss.js
@@ -0,0 +1,12 @@
+import * as tailwindComponents from 'vue-tailwind/dist/components';
+
+const {
+    // Some Tailwind Components Name, e.g. TTag
+    ...defaultComponents
+} = tailwindComponents;
+const components = {
+    // Some Tailwind Components Config
+    ...defaultComponents
+};
+
+export default components;
