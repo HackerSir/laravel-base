ac8af940dcfcf42f5d2fba585f27b583b0d03d0e
Vongola <vongola12324@coder.tw>
2021-02-23 11:35:38 +0800

    Bump laravel-mix to 6.x

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 let buildJsPath = 'public/build-js';
 let buildCssPath = 'public/build-css';
 
-mix.js('resources/js/app.js', buildJsPath).version();
+mix.js('resources/js/app.js', buildJsPath).version().vue();
 // mix.sass('resources/sass/app.scss', buildCssPath);
 mix.stylus('resources/stylus/app.styl', buildCssPath).version();
 
