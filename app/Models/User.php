7bbb95ca18a5fb17fc9a35392a88a34fa96fc80f
KID <jyhsu2000@gmail.com>
2021-07-11 16:25:20 +0800

    Change primary key of User to UUID

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Traits\UuidPrimaryKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use UuidPrimaryKey;
     use HasFactory, Notifiable;
 
     /**
